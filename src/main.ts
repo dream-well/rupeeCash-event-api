cacd2bf1114263bdc18123e909aa9636cc0e130b
dream-well <dreamwell506@outlook.com>
2022-11-30 22:53:48 +0800

    drop db cmd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,4 +10,16 @@ async function bootstrap() {
   await app.listen(8000);
 }
 
-bootstrap();
+switch(process.env.mode) {
+  case "db-drop":
+    mongoose.connect('mongodb://localhost/subchain',function(){
+        /* Drop the DB */
+        console.log(mongoose.connection.db.databaseName);
+        mongoose.connection.db.dropDatabase();
+        process.exit(0);
+    });
+    break;
+  default:
+    bootstrap();
+}
+
